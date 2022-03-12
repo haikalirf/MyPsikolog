2ad170c4f7af5270bdb44469aab5d947f2119dc3
Bleh-Blah <haikalirfano@gmail.com>
2022-03-12 14:31:45 +0700

    Added  a RecylerView for main chat

--- app/src/main/java/com/example/mypsikolog/MainActivity.kt
+++ app/src/main/java/com/example/mypsikolog/MainActivity.kt
@@ -5,17 +5,28 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import com.example.mypsikolog.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnChatButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnChatButton = findViewById(R.id.btn_chatButton_activity_main)
 
         val buttonSuicideHotline = findViewById<Button>(R.id.btn_suicideHotline)
         buttonSuicideHotline.setOnClickListener{
             val intent = Intent(Intent.ACTION_DIAL)
             intent.data = Uri.parse("tel:021500454")
+            startActivity(intent)
+        }
+
+        btnChatButton.setOnClickListener {
+            val intent = Intent(this, MainChatActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/mypsikolog/MainChatActivity.kt
+++ app/src/main/java/com/example/mypsikolog/MainChatActivity.kt
@@ -2,11 +2,19 @@ package com.example.mypsikolog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainChatActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_chat)
-        //TODO
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
     }
 }
